a8c693467df3bce504d806b6e594ee53fa32b68f
Ashish Kumar <ashishjohnexx786@gmail.com>
2026-09-02 13:51:48 +0530

    Upgrade Engineering Mentor to 2026 v2.0

--- course-sync.js
+++ course-sync.js
@@ -1,5 +1,5 @@
 (()=>{
-  const RECOMMENDED=new Set(["C3-00-L02","C3-00-L04","C3-00-L11","C3-02-L03","C3-02-L10","C3-03-L02","C3-03-L03","C3-03-L04","C3-03-L06","C3-03-L07","C3-03-L08","C3-03-L10","C3-03-L12","C3-04-L08","C3-04-L11","C3-05-L01","C3-05-L02","C3-05-L10","C3-05-L11","C3-05-L13","C3-05-L14","C3-06-L01","C3-06-L02","C3-06-L03","C3-06-L04","C3-06-L05","C3-06-L06","C3-06-L08","C3-06-L09","C3-06-L10","C3-06-L11","C3-06-L13","C3-07-L03","C3-07-L04","C3-07-L05","C3-07-L06","C3-07-L07","C3-07-L08","C3-07-L09","C3-07-L10","C3-07-L12","C3-07-L13","C3-07-L14","C3-07-L15"]);
+  const RECOMMENDED=new Set(["C3-01-L01","C3-01-L02","C3-01-L03","C3-01-L08","C3-01-L09","C3-01-L10","C3-01-L11","C3-02-L01","C3-02-L02","C3-02-L03","C3-02-L12","C3-02-L13","C3-03-L05","C3-03-L06","C3-03-L07","C3-04-L09","C3-05-L03","C3-05-L05","C3-05-L06","C3-05-L09","C3-05-L11","C3-05-L12","C3-06-L10","C3-07-L04","C3-07-L06","C3-07-L07","C3-07-L09","C3-08-L03","C3-09-L13"]);
   const status=id=>RECOMMENDED.has(id)?"☑ Recommended visual":"☐ Not required";
   function applyData(){if(typeof COURSE3!=="undefined")COURSE3.forEach(p=>p.lessons.forEach(l=>l.videoStatus=status(l.id)));}
   function decorate(){
